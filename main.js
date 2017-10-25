c8d8fd6f765215c17e7d6eccf65c84c539a4d643
Ilyas Ahsan <meilyasahsan@users.noreply.github.com>
2017-10-25 10:38:48 +0700

    hide some credential :v

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var apiai = require('apiai');
 
-var app = apiai('f98941cda5294818be3f04dfbf5108ab');
+var app = apiai('<client token>');
 
 var request = app.textRequest('weather today on jakarta', {
-  sessionId:  'b31e89d2-8e27-4ef8-a133-8d95be6bb2bc'
+  sessionId:  '<session id>'
 });
 
 request.on('response',  function(response){
